04a1d129059e5fc96a303ff81d984ec8e62e86ff
dell <jchhura21@gmail.com>
2022-06-16 17:04:27 +0530

    completed

--- app/src/main/java/com/example/contactapi/model/Listeners.kt
+++ app/src/main/java/com/example/contactapi/model/Listeners.kt
@@ -1,7 +1,10 @@
 package com.example.contactapi.model
 
+import android.database.sqlite.SQLiteOpenHelper
+
 interface Listeners {
     fun onClickDelete(position:Int,userId:Int)
 
     fun onClickUpdate(position:Int,userId:Int,name:String,Phone:Long)
 }
+
